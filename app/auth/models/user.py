3fb28b2ba2407e9e10f84f57844049b7c8ea6b92
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2021-08-13 17:47:23 +0300

    user model

--- app/auth/models/user.py
+++ app/auth/models/user.py
@@ -1,3 +1,4 @@
+from app.main.models import company
 import base64
 from datetime import datetime, timedelta
 from hashlib import md5
@@ -28,9 +29,86 @@ import logging
 
 class User(UserMixin, PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    active = db.Column(db.Boolean, default=True)
-    password_hash = db.Column(db.String(128))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+    company = db.relationship('Company', backref='user_company', uselist=False)
+    is_active = db.Column(db.Boolean, default=True)
     name = db.Column(db.String(128), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    phone_no = db.Column(db.String(120), index=True, unique=True)
+    is_staff = db.Column(db.Boolean, default=True)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    registered_on = db.Column(db.DateTime, default=datetime.utcnow)
+    phone_no = db.Column(db.String(120), index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'token': self.username,
+            'token_expiration': self.token_expiration,
+            'company_id': self.company_id,
+            'is_active': self.is_active,
+            'name': self.name,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            'registered_on': self.registered_on.isoformat() + 'Z',
+            'is_staff': self.is_staff,
+            '_links': {
+                'self': url_for('api.get_user', id=self.id)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['name', 'email', 'token', 'token_expiration', 'company_id']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
