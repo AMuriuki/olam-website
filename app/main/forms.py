f7d27d932da66f5e5cb33bede434be15720a4c0b
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2021-08-13 17:48:14 +0300

    get_started form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from app.main.models.company import Company
 from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
@@ -15,14 +16,7 @@ class GetStartedForm(FlaskForm):
                               validators=[DataRequired()])
     companyname = StringField(_l('Company Name'),
                               validators=[DataRequired()])
-    # domain = StringField(_l('Domain'),
-    #                      validators=[DataRequired()])
     domainoutput = StringField(_l('Domain'),
                                validators=[DataRequired(), Regexp(r'^[\w.@+-]+$', message="Spaces are not allowed in domain names")], render_kw={'readonly': True})
     submit = SubmitField(_l('Start Now'))
 
-    # def validate_domainoutput(self, email):
-    #     domain = Domain.query.join(User, Domain.user_id == User.id).filter(
-    #         User.email == email.data).first()
-    #     if domain is not None:
-    #         raise ValidationError(_('Please use a different name.'))
