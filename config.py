2f7cdd0c35501824bfe90bd41ecf17799e761ccc
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2021-08-18 16:33:03 +0300

    updates

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'olam.db')
-    SQLALCHEMY_BINDS = {
-
-    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
