aecdfc8400296c2d8242b0751f298e4b4d0ab50d
Arnold Nderitu <arnoldnderitu@gmail.com>
2021-09-07 21:04:40 +0300

    requirements with ansible commented out

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,7 +13,7 @@ from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app, session, request, abort
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
-from guess_language import guess_language
+# from guess_language import guess_language
 from app import db, call_ansible
 from app.main import bp
 from app.main.forms import GetStartedForm
