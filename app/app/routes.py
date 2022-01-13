636109adf0670b79bc147d5473ed6cfcd7696fac
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-01-13 20:52:44 +0300

    features

--- app/app/routes.py
+++ app/app/routes.py
@@ -2,6 +2,8 @@ from app.app import bp
 from flask.templating import render_template
 from flask_babel import _, get_locale
 
+from app.main.models.module import FeatureCategory, Module, ModuleFeature
+
 
 @bp.route("/<app_name>")
 def view(app_name):
@@ -11,8 +13,17 @@ def view(app_name):
 
 @bp.route('/<app_name>/features')
 def features(app_name):
+    feature_ids = []
     if app_name:
         app_name = app_name
+        module = Module.query.filter_by(bp_name=app_name).first()
+        features = ModuleFeature.query.filter_by(
+            module_id=module.id).all()
+        for feature in features:
+            feature_ids.append(feature.id)
+        
+        categories = FeatureCategory.query.filter(
+            FeatureCategory.id.in_(feature_ids)).all()
     else:
         app_name = None
-    return render_template("features.html", app_name=app_name, title=_("Olam " + app_name + " | Olam ERP"))
+    return render_template("features.html", app_name=app_name, title=_("Olam " + app_name + " | Olam ERP"), features=features, categories=categories)
